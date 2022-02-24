78b930ddb2247198196d907641e2fa50ccec4737
lcleary <luke.cleary@copenhagenatomics.com>
2022-02-24 18:55:47 +0100

    moab PKGBUILD complete

--- Arch/AUR/install-all.sh
+++ Arch/AUR/install-all.sh
@@ -2,51 +2,42 @@
 set -ex
 
 #install needed base-devel software & turn off install confirms
-#echo 'Defaults    timestamp_timeout=-1' | sudo EDITOR='tee -a' visudo
 sudo pacman -Syy --ignore sudo --needed base-devel
 
 # Compile MOAB package and install it
 cd MOAB-PKGBUILD
-makepkg --noconfirm -cs -g >> PKGBUILD && makepkg
-sudo pacman -U ./moab*
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
 echo "compiled & installed MOAB, proceeding..."
 
 # Compile Embree package and install it
 cd EMBREE-PKGBUILD
-makepkg --noconfirm -cs
-sudo pacman -U ./embree*
+sudo pacman -Syy --noconfirm tbb openimageio
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
 echo "compiled & installed Embree, proceeding..."
 
 # Compile double-down package and install it
 cd DOUBLE-DOWN-PKGBUILD
-makepkg --noconfirm -cs
-sudo pacman -U ./double-down*
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
 echo "compiled & installed double-down, proceeding..."
 
 # Compile DAGMC package and install it
 cd DAGMC-PKGBUILD
-makepkg --noconfirm -cs
-sudo pacman -U ./dagmc*
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
 echo "compiled & installed DAGMC, proceeding..."
 
 # Compile OpenMC package and install it
 cd NUCLEAR-DATA-PKGBUILD
-makepkg --noconfirm -cs
-sudo pacman -U ./nuclear-data*
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
 echo "nuclear data downloaded , proceeding..."
 
 # Compile OpenMC package and install it
 cd OPENMC-PKGBUILD
-makepkg --noconfirm -cs
-sudo pacman -U ./openmc*
+makepkg --noconfirm -csgi >> PKGBUILD && makepkg
 cd ..
-
-#reset sudoers file
-#sudo sed '$d' -i /etc/sudoers
 
 echo "compiled & installed OpenMC, finised!"
