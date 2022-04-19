d55fbf77cd21cd886eb582bf7d44e613c64480e3
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-04-19 09:35:57 +0200

    scripts for ubuntu

---
+++ Ubuntu_20.04/install-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+echo 'Defaults    timestamp_timeout=-1' | sudo EDITOR='tee -a' visudo
+
+#openmc compile & install
+#openmc-install.sh will call install scripts of its dependencies & nuclear data
+./openmc-install.sh
+echo "Compiled & installed openmc, done."
+
+echo "Running test script..."
+python test_openmc.py
+
+#remove timestamp update
+sudo sed -i '/Defaults    timestamp_timeout=-1/d' /etc/sudoers
---
+++ Ubuntu_20.04/openmc-install.sh
@@ -0,0 +1,48 @@
+################################################################################
+#openmc source install
+################################################################################
+#!/bin/bash
+set -ex
+
+#nuclear_data_download
+#./nuclear_data-install.sh
+#echo "Downloaded & extracted nuclear data, proceeding..."
+  
+#dagmc compile & install
+./dagmc-install.sh
+echo "Compiled & installed dagmc, proceeding..."
+
+sudo apt-get install --yes libpng-dev libpng++-dev\
+	imagemagick\
+	python3-lxml\
+        python3-scipy\
+        python3-pandas\
+        python3-h5py\
+        python3-matplotlib\
+        python3-uncertainties 
+
+#if there is a .done-file then skip this step
+if [ ! -e $0.done ]; then
+  #source install
+  cd $HOME
+  mkdir -p openmc_chkout
+  cd openmc_chkout
+  git clone --recurse-submodules --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
+  cd openmc
+
+
+  mkdir -p build
+  cd build
+  cmake -DOPENMC_USE_DAGMC=ON \
+           -DDAGMC_ROOT=$HOME/openmc/DAGMC \
+           -DHDF5_PREFER_PARALLEL=off ..
+  sudo make install
+  cd ..
+  sudo pip3 install .
+  rm -rf build
+  #this was apparently successful - mark as done. 
+  touch ${0}.done
+else
+   name=`basename $0`
+   echo openmc appears to already be installed \(lock file ${name}.done exists\) - skipping.
+fi
