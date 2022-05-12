5724f6e5fcf6d10cdbd1cd70710b92133993fa32
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-05-12 10:47:54 +0200

    allow aparallel build and create lock-files
    the lock file helps avoid unecessary reompilation

--- Arch/openmc-install.sh
+++ Arch/openmc-install.sh
@@ -4,31 +4,65 @@
 #!/bin/bash
 set -ex
 
+#nuclear_data_download
+#./nuclear_data-install.sh
+#echo "Downloaded & extracted nuclear data, proceeding..."
+
 #dagmc compile & install
 ./dagmc-install.sh
 echo "Compiled & installed dagmc, proceeding..."
 
-#nuclear_data_download
-./nuclear_data-install.sh
-echo "Downloaded & extracted nuclear data, proceeding..."
+WD=`pwd`
+name=`basename $0`
+install_prefix="/opt"
+build_prefix="$HOME/openmc"
 
-sudo pacman -Syu --noconfirm \
-	python-lxml \
-	python-scipy \
-	python-pandas \
-	python-h5py \
-	python-matplotlib \
+#if there is a .done-file then skip this step
+if [ ! -e ${name}.done ]; then
+  sudo pacman -Syu --noconfirm\
+	python-lxml\
+	python-scipy\
+	python-pandas\
+	python-h5py\
+	python-matplotlib\
 	python-uncertainties
 
-#source install
-cd /opt
-sudo git clone --recurse-submodules --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
-cd openmc
-sudo mkdir build
-cd build
-cmake -DOPENMC_USE_DAGMC=ON \
-           -DDAGMC_ROOT=/opt/DAGMC \
-           -DHDF5_PREFER_PARALLEL=off ..
-sudo make install
-cd /opt/openmc/
-sudo python setup.py install
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  #source install
+  mkdir -p $HOME/openmc
+  cd $HOME/openmc
+  if [ -e openmc ]; then
+        cd openmc
+        git pull --recurse-submodules
+  else
+        git clone --recurse-submodules --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
+        cd openmc
+  fi
+
+  if [ -e build ]; then
+    rm -rf build.bak
+    mv build build.bak
+  fi
+  mkdir -p build
+  cd build
+  cmake -DOPENMC_USE_DAGMC=ON \
+        -DDAGMC_ROOT=${install_prefix}/DAGMC \
+        -DHDF5_PREFER_PARALLEL=off ..
+  make -j $ccores
+  sudo make install
+
+  cd ..
+  sudo pip3 install .
+
+  cd ${WD}
+
+  #this was apparently successful - mark as done.
+  touch ${name}.done
+else
+  echo openmc appears to be already installed \(lock file ${name}.done exists\) - skipping.
+fi
