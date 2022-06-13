ee91820c0c271a9f7541b36d36a5022f83371514
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-06-13 09:08:29 +0200

    add versions for 22.04

---
+++ Ubuntu_22.04/dagmc-install.sh
@@ -0,0 +1,45 @@
+################################################################################
+#dagmc source install
+################################################################################
+#!/bin/bash
+set -ex
+
+#double-down compile & install
+./double_down-install.sh
+echo "Compiled & installed double-down, proceeding..."
+
+WD=`pwd`
+name=`basename $0`
+package_name='dagmc'
+
+#if there is a .done-file then skip this step
+if [ ! -e ${name}.done ]; then
+
+  sudo apt-get install --yes python3
+
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  mkdir -p $HOME/openmc/DAGMC
+  cd $HOME/openmc/DAGMC
+  git clone --single-branch --branch develop --depth 1 https://github.com/svalinn/DAGMC.git
+  mkdir build
+  cd build
+  cmake ../DAGMC -DBUILD_TALLY=ON \
+               -DMOAB_DIR=$HOME/openmc/MOAB \
+               -DDOUBLE_DOWN=ON \
+               -DBUILD_STATIC_EXE=OFF \
+               -DBUILD_STATIC_LIBS=OFF \
+               -DCMAKE_INSTALL_PREFIX=$HOME/openmc/DAGMC/ \
+               -DDOUBLE_DOWN_DIR=$HOME/openmc/double-down
+  make -j $ccores
+  make install
+
+  cd ${WD}
+  touch ${name}.done
+else
+  echo DAGMC appears already to be installed \(lock file ${name}.done exists\) - skipping.
+fi
---
+++ Ubuntu_22.04/embree-install.sh
@@ -0,0 +1,49 @@
+################################################################################
+#embree source install
+################################################################################
+#!/bin/bash
+set -ex
+if [ "x" == "$1x" ]; then
+	ccores=1
+else
+	ccores=$1
+fi
+
+sudo apt-get install --yes gcc\
+       	build-essential\
+	cpp\
+        make\
+        cmake\
+        libglfw3\
+        libglfw3-dev\
+        libtbb2\
+        libtbb-dev\
+        libopenimageio2.1\
+        libopenimageio-dev
+
+sudo pip install numpy
+WD=`pwd`
+
+#if there is a .done-file then skip this step
+if [ ! -e $0.done ]; then
+   cd $HOME
+   mkdir -p openmc
+   cd openmc
+   mkdir -p embree
+   cd embree
+   git clone --single-branch --branch v3.13.3 --depth 1 https://github.com/embree/embree.git
+   mkdir build
+   cd build
+   cmake ../embree -DCMAKE_INSTALL_PREFIX=$HOME/openmc/embree \
+                -DEMBREE_ISPC_SUPPORT=OFF \
+		-DEMBREE_TUTORIALS=OFF
+   make -j $ccores
+   sudo make install
+   rm -rf embree/build embree/embree
+
+   cd ${WD}
+   touch ${0}.done
+else
+   name=`basename $0`
+   echo embree appears to already be installed \(lock file ${name}.done exists\) - skipping.
+fi
