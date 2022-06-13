8d73e20e69f281f6a104a5cce2314b1796428b86
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-06-13 09:08:29 +0200

    add versions for 22.04

---
+++ Ubuntu_22.04/double_down-install.sh
@@ -0,0 +1,45 @@
+################################################################################
+#double-down source install
+################################################################################
+#!/bin/bash
+set -ex
+
+#embree compile & install
+./embree-install.sh
+echo "Compiled & installed embree, proceeding..."
+
+#moab compile & install
+./moab-install.sh
+echo "Compiled & installed moab, proceeding..."
+
+WD=`pwd`
+name=`basename $0`
+package_name='double_down'
+
+#if there is a .done-file then skip this step
+if [ ! -e ${name}.done ]; then
+  sudo apt-get install --yes doxygen\
+        libembree3-3 libembree-dev
+
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  mkdir -p $HOME/openmc/double-down
+  cd $HOME/openmc/double-down
+  git clone --single-branch --branch main --depth 1 https://github.com/pshriwise/double-down.git
+  mkdir build
+  cd build
+  cmake ../double-down -DMOAB_DIR=$HOME/openmc/MOAB \
+                     -DCMAKE_INSTALL_PREFIX=$HOME/openmc/double-down
+
+  make -j $ccores
+  make install
+
+  cd ${WD}
+  touch ${name}.done
+else
+  echo double-down appears to be already installed \(lock file ${name}.done exists\) - skipping.
+fi
---
+++ Ubuntu_22.04/nuclear_data-install.sh
@@ -0,0 +1,34 @@
+################################################################################
+#nuclear data download & extract
+################################################################################
+#!/bin/bash
+set -ex
+
+if [ ! -e $0.done ]; then
+ if [ "x${OPENMC_CROSS_SECTIONS}"=="x" ]; then
+	 echo "export OPENMC_CROSS_SECTIONS=$HOME/openmc/nuclear_data/mcnp_endfb71/cross_sections.xml" >> $HOME/.bashrc
+ fi
+
+ #default libraries
+ endfb_VII="https://anl.box.com/shared/static/d359skd2w6wrm86om2997a1bxgigc8pu.xz"
+ endfb_VIII="https://anl.box.com/shared/static/nd7p4jherolkx4b1rfaw5uqp58nxtstr.xz"
+ jeff="https://anl.box.com/shared/static/ddetxzp0gv1buk1ev67b8ynik7f268hw.xz"
+
+ mkdir -p $HOME/openmc/nuclear_data
+ cd $HOME/openmc/nuclear_data
+
+ if [ ! -e `basename $endfb_VII` ]; then
+   #see other default options above
+   wget $endfb_VII
+
+   for entry in "$PWD"/*
+   do
+     tar -xvf $entry
+   done
+ fi
+  touch ${0}.done
+else
+   name=`basename $0`
+   echo nuclear data appears to already be installed \(lock file ${name}.done exists\) - skipping.
+fi
+
