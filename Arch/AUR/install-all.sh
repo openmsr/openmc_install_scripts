d8091efb15e32b703eb6b932d1adfe84ba64f212
LukeLabrie <lplcleary@gmail.com>
2022-11-08 18:09:27 +0100

    further updates to AUR scripts

--- Arch/AUR/install-all.sh
+++ Arch/AUR/install-all.sh
@@ -6,10 +6,14 @@ find /var/cache/pacman/pkg/ -iname "*.part" -delete
 #sudo pacman -Syyu --ignore sudo --needed base-devel
 
 # Compile MOAB package and install it
-cd MOAB-PKGBUILD
-makepkg --noconfirm -csgi >> PKGBUILD && makepkg
-cd ..
-echo "compiled & installed MOAB, proceeding..."
+if [ ! -e ./MOAB-PKGBUILD/*.tar.zst ]; then
+    cd MOAB-PKGBUILD
+    makepkg --noconfirm -csgi >> PKGBUILD && makepkg
+    cd ..
+    echo "compiled & installed MOAB, proceeding..."
+else
+    echo "moab already installed, proceeding..."
+fi
 
 # embree is now available via pacman
 # Compile Embree package and install it
@@ -19,26 +23,43 @@ echo "compiled & installed MOAB, proceeding..."
 #echo "compiled & installed Embree, proceeding..."
 
 # Compile double-down package and install it
-cd DOUBLE-DOWN-PKGBUILD
-makepkg --noconfirm -csgi >> PKGBUILD && makepkg
-cd ..
-echo "compiled & installed double-down, proceeding..."
+if [ ! -e ./DOUBLE-DOWN-PKGBUILD/*.tar.zst ]; then
+    cd DOUBLE-DOWN-PKGBUILD
+    makepkg --noconfirm -csgi >> PKGBUILD && makepkg
+    cd ..
+    echo "compiled & installed double-down, proceeding..."
+else
+    echo "double-down already installed, proceeding..."
+fi
 
 # Compile DAGMC package and install it
-cd DAGMC-PKGBUILD
-makepkg --noconfirm -csgi >> PKGBUILD && makepkg
-cd ..
-echo "compiled & installed DAGMC, proceeding..."
+if [ ! -e ./DAGMC-PKGBUILD/*.tar.zst ]; then
+    cd DAGMC-PKGBUILD
+    makepkg --noconfirm -csgi >> PKGBUILD && makepkg
+    cd ..
+    echo "compiled & installed DAGMC, proceeding..."
+else
+    echo "dagmc already installed, proceeding..."
+fi
 
 # Download the nuclear data
-cd NUCLEAR-DATA-PKGBUILD
-makepkg --noconfirm -csgi >> PKGBUILD && makepkg
-cd ..
-echo "nuclear data downloaded , proceeding..."
+if [ ! -e ./NUCLEAR-DATA-PKGBUILD/*.tar.zst ]; then
+    cd NUCLEAR-DATA-PKGBUILD
+    makepkg --noconfirm -csgi >> PKGBUILD && makepkg
+    cd ..
+    echo "nuclear data downloaded , proceeding..."
+else
+    echo "nuclear data already installed, proceeding..."
+fi
 
 # Compile OpenMC package and install it
-cd OPENMC-PKGBUILD
-makepkg --noconfirm -csgi >> PKGBUILD && makepkg
-cd ..
-echo "compiled & installed OpenMC, finised!"
-python test_openmc.py
+if [ ! -e ./OPENMC-PKGBUILD/*.tar.zst ]; then
+    cd OPENMC-PKGBUILD
+    makepkg --noconfirm -csgi >> PKGBUILD && makepkg
+    cd ..
+    echo "compiled & installed OpenMC, finised!"
+    python tests/step_to_h5m.py
+    python tests/test_openmc.py
+else
+    echo "openmc already installed"
+fi
