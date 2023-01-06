c1e45beea99fc7f2a46e0d196ab12a3700a0b2af
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-01-06 12:24:11 +0100

    update to reflect Arch

--- Debian11/openmc-install.sh
+++ Debian11/openmc-install.sh
@@ -8,19 +8,30 @@ set -ex
 #./nuclear_data-install.sh
 #echo "Downloaded & extracted nuclear data, proceeding..."
 
+openmc_version="v0.13.2"
+if [ "x" != "x$OPENMC_VERSION" ]; then
+	openmc_version=$OPENMC_VERSION
+fi
+
+
 #dagmc compile & install
 ./dagmc-install.sh
 echo "Compiled & installed dagmc, proceeding..."
 
 WD=`pwd`
 name=`basename $0`
-install_prefix="/opt"
+install_prefix="/usr/local/lib"
 if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
   install_prefix=$LOCAL_INSTALL_PREFIX
 fi
-build_prefix="$HOME/openmc"
+
+build_prefix="/dev/null/openmc" #this will never exist - and so use the default later.
+if [ "x" != "x$OPENMC_BUILD_PREFIX" ]; then
+  build_prefix=$OPENMC_BUILD_PREFIX
+fi
 
 echo will install openmc to $install_prefix
+echo will build openmc from $build_prefix
 
 #if there is a .done-file then skip this step
 if [ ! -e ${name}.done ]; then
@@ -39,15 +50,28 @@ if [ ! -e ${name}.done ]; then
 	ccores=$1
   fi
 
-  #source install
-  mkdir -p $HOME/openmc
-  cd $HOME/openmc
-  if [ -e openmc ]; then
-        cd openmc
-        git pull --recurse-submodules
+  #Should --openmc_build be passed as argument, it assumes a git version is already checked-out
+  if [ -e $build_prefix/openmc ]; then
+    cd $build_prefix/openmc
   else
-        git clone --recurse-submodules --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
-        cd openmc
+    #source install
+  	mkdir -p $HOME/openmc
+  	cd $HOME/openmc
+  	if [ -e openmc ]; then
+      #repo exists checkout the given version and get new updates
+      #(updates are of course only relevant for development branches.)
+      cd openmc
+      git checkout $openmc_version
+      #if this is a branch - make sure it is up to date
+      if git show-ref --verify refs/heads/$openmc_version; then
+        git pull
+      fi
+    else
+      #clone the repo and checkout the given version
+      git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
+      cd openmc
+      git checkout $openmc_version
+  	fi
   fi
 
   if [ -e build ]; then
