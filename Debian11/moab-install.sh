8874fe4bcbc80669f0fc9dc73b3b69b38a9da6e5
erkn <erik@vonbrun.org>
2023-02-01 11:17:23 +0100

    pymoab was enabled 2x

--- Debian11/moab-install.sh
+++ Debian11/moab-install.sh
@@ -54,7 +54,6 @@ if [ ! -e ${name}.done ]; then
   mkdir -p build
   cd build
   cmake ../moab -DENABLE_HDF5=ON \
-              -DENABLE_PYMOAB=ON \
               -DENABLE_NETCDF=ON \
 	      -DENABLE_PYMOAB=ON \
               -DENABLE_FORTRAN=OFF \
