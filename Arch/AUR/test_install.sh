d0723cd6538ea36b6a6c60b548ea52551d4fa582
LukeLabrie <lplcleary@gmail.com>
2023-02-06 15:53:06 +0100

    permission fix on nuclear data

--- Arch/AUR/test_install.sh
+++ Arch/AUR/test_install.sh
@@ -1,14 +1,15 @@
 # set non-standard paths
 export PATH=$PATH:/opt/openmc/bin:$HOME/.local/lib/python3.10/site-packages
-var=`ls /opt/*hdf5|head -n1`
-echo $var
-export OPENMC_CROSS_SECTIONS=$var
+var=`echo /opt/nuclear-data/*hdf5 | head -n1`
+export OPENMC_CROSS_SECTIONS=$var/cross_sections.xml
+echo $OPENMC_CROSS_SECTIONS
+
 
 # test geometry build
-python tests/step_to_h5m.py
+#python tests/step_to_h5m.py
 
 # test run
 python tests/test_openmc.py
 
 # clean
-rm *.stl
+#rm *.stl
