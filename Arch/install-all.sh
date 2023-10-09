48de559abf6269bdb4fe5893aadb09d3612cfca6
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-10-09 15:58:35 +0200

    cleanup and copy over things to Ubuntu

--- Arch/install-all.sh
+++ Arch/install-all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # set -ex
 
-#echo 'Defaults    timestamp_timeout=-1' | sudo EDITOR='tee -a' visudo
-
 #default install location. may be overrridden by the option --prefix=<path>
 LOCAL_INSTALL_PREFIX=/usr/local/lib
 
