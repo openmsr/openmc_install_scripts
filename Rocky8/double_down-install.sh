e98adae9a8cbbb1f917ac73debdae066b56c7cb6
Erik B Knudsen <erik@vonbrun.org>
2024-09-19 17:11:47 +0200

    initial version

---
+++ Rocky8/double_down-install.sh
@@ -0,0 +1,65 @@
+################################################################################
+#double-down source install
+################################################################################
+#!/bin/bash
+set -ex
+
+#embree compile & install
+#./embree-install.sh
+#echo "Compiled & installed embree, proceeding..."
+
+#moab compile & install
+./moab-install.sh
+echo "Compiled & installed moab, proceeding..."
+
+WD=`pwd`
+name=`basename $0`
+package_name='double_down'
+
+install_prefix="/usr/local/lib"
+if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
+  install_prefix=$LOCAL_INSTALL_PREFIX
+fi
+build_prefix="$HOME"
+if [ "x" != "x$OPENMC_BUILD_PREFIX" ]; then
+  build_prefix=$OPENMC_BUILD_PREFIX
+fi
+
+build_type="Release"
+if [ "xON" = "x$DEBUG_BUILD" ]; then
+    build_type="Debug"
+fi
+sudo apt-get install --yes doxygen\
+        libembree3-3 libembree-dev
+
+
+#if there is a .done-file then skip this step
+if [ ! -e ${name}.done ]; then
+  sudo apt-get install --yes doxygen\
+        libembree3-3 libembree-dev
+
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep processor|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  mkdir -p ${build_prefix}/openmc/double-down
+  cd ${build_prefix}/openmc/double-down
+  if [ ! -d double-down ]; then
+    git clone --single-branch --branch develop --depth 1 https://github.com/pshriwise/double-down.git
+  fi
+
+  mkdir -p build
+  cd build
+  cmake ../double-down -DMOAB_DIR=${install_prefix}\
+            -DCMAKE_BUILD_TYPE=${build_type}\
+            -DCMAKE_INSTALL_PREFIX=${install_prefix}
+  make -j ${ccores}
+  make install
+
+  cd ${WD}
+  touch ${name}.done
+else
+  echo double-down appears to be already installed \(lock file ${name}.done exists\) - skipping.
+fi
---
+++ Rocky8/openmc-install.sh
@@ -0,0 +1,113 @@
+################################################################################
+#openmc source install
+################################################################################
+#!/bin/bash
+set -ex
+
+#nuclear_data_download
+#./nuclear_data-install.sh
+#echo "Downloaded & extracted nuclear data, proceeding..."
+
+openmc_version="v0.15.0"
+if [ "x" != "x$OPENMC_VERSION" ]; then
+	openmc_version=$OPENMC_VERSION
+fi
+
+
+#dagmc compile & install
+./dagmc-install.sh
+echo "Compiled & installed dagmc, proceeding..."
+
+WD=`pwd`
+name=`basename $0`
+package_name='openmc'
+install_prefix="/usr/local/lib"
+if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
+  install_prefix=$LOCAL_INSTALL_PREFIX
+fi
+
+build_prefix="$HOME"
+if [ "x" != "x$OPENMC_BUILD_PREFIX" ]; then
+  build_prefix=$OPENMC_BUILD_PREFIX
+fi
+
+build_type="Release"
+if [ "xON" = "x$DEBUG_BUILD" ]; then
+    build_type="Debug"
+fi
+
+echo will install openmc to $install_prefix
+echo will build openmc from $build_prefix
+
+#if there is a .done-file then skip this step
+if [ ! -e ${name}.done ]; then
+  sudo apt-get install libpng-dev libpng++-dev\
+	imagemagick\
+        python3-lxml\
+        python3-scipy\
+        python3-pandas\
+        python3-h5py\
+        python3-h5py-mpi\
+        python3-matplotlib\
+        python3-uncertainties
+
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep processor|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  #Should --openmc_build be passed as argument, it assumes a git version is already checked-out
+  if [ -e ${build_prefix}/openmc/openmc ]; then
+    cd ${build_prefix}/openmc/openmc
+  else
+    #source install
+    mkdir -p ${build_prefix}/openmc
+    cd ${build_prefix}/openmc
+    if [ -e openmc ]; then
+      #repo exists checkout the given version and get new updates
+      #(updates are of course only relevant for development branches.)
+      cd openmc
+      git checkout $openmc_version
+      #if this is a branch - make sure it is up to date
+      if git show-ref --verify refs/heads/$openmc_version; then
+        git pull
+      fi
+    else
+      #clone the repo and checkout the given version
+      git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
+      cd openmc
+      git checkout $openmc_version
+    fi
+  fi
+
+  #do a source install of MCPL
+  ./tools/ci/gha-install-mcpl.sh
+
+  if [ -e build ]; then
+    rm -rf build.bak
+    mv build build.bak
+  fi
+  mkdir -p build
+  cd build
+  cmake -DOPENMC_USE_DAGMC=ON\
+        -DOPENMC_USE_OPENMP=ON\
+        -DOPENMC_USE_MPI=ON\
+        -DOPENMC_USE_MCPL=ON\
+        -DCMAKE_BUILD_TYPE=${build_type}\
+        -DDAGMC_ROOT=${install_prefix}\
+        -DHDF5_PREFER_PARALLEL=off\
+	-DCMAKE_INSTALL_PREFIX=${install_prefix} ..
+  make -j $ccores
+  make install
+
+  #install the python layer
+  pip install .. --prefix=${install_prefix}
+
+  cd ${WD}
+
+  #this was apparently successful - mark as done.
+  touch ${name}.done
+else
+  echo openmc appears to be already installed \(lock file ${name}.done exists\) - skipping.
+fi
